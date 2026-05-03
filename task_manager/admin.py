46c25205f7daac196a08ce1c76a16320c0a03916
Gongzuo-Dk <gongzuo.dk@gmail.com>
2026-05-03 20:01:23 +0300

    Registered admin models

--- task_manager/admin.py
+++ task_manager/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
+from .models import Category, Priority, Task
 
-# Register your models here.
+@admin.register(Task)
+class TaskAdmin(admin.ModelAdmin):
+    list_display = ("title", "category", "priority", "due_date")
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    pass
+
+@admin.register(Priority)
+class PriorityAdmin(admin.ModelAdmin):
+    pass
