437174d663d74e9a7a124e0dbf5d60f4ab14a2b8
Gongzuo-Dk <gongzuo.dk@gmail.com>
2026-05-05 16:43:35 +0300

    Implement user authentication: register, login, logout

--- task_manager/views.py
+++ task_manager/views.py
@@ -1,12 +1,13 @@
 from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
 from task_manager.models import Task, Category, Priority
 
 # Create your views here.
 
+@login_required
 def index(request):
-    all_tasks = Task.objects.order_by("-created_at")
+    all_tasks = Task.objects.filter(user=request.user).order_by("-created_at")
     context = {
         "all_tasks": all_tasks,
     }
-
     return render(request, "task_manager/index.html", context)
